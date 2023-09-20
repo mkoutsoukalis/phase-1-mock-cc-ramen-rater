90032716ab857c91cdafc01ef55cb1e13a8d2bcc
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-09-20 11:59:00 -0400

    Core-1-Only

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 // write your code here
+/*See all ramen images in the `div` 
+with the id of `ramen-menu`. When the page */
+fetch("http://localhost:3000/ramens")
+    .then(res => res.json())
+    .then(images => {
+        images.forEach((ramen) => {
+            const ramenContent = document.createElement("img");
+            ramenContent.src = ramen.image;
+            document.querySelector("#ramen-menu").append(ramenContent);
+        })
+    });
